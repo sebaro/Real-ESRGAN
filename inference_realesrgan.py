1b821eaade4ea961f5f14f3334aa20f365a6d7b3
sebaro <esebaro@gmail.com>
2024-07-01 21:43:17 +0300

    my changes

--- inference_realesrgan.py
+++ inference_realesrgan.py
@@ -14,29 +14,38 @@ def main():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', type=str, default='inputs', help='Input image or folder')
+    parser.add_argument('-o', '--output', type=str, default='outputs', help='Output folder')
     parser.add_argument(
         '-n',
         '--model_name',
         type=str,
         default='RealESRGAN_x4plus',
-        help=('Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus | '
-              'realesr-animevideov3 | realesr-general-x4v3'))
-    parser.add_argument('-o', '--output', type=str, default='results', help='Output folder')
+        help='Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus | '
+              'realesr-animevideov3 | realesr-general-x4v3. Default: RealESRGAN_x4plus')
     parser.add_argument(
         '-dn',
         '--denoise_strength',
         type=float,
         default=0.5,
-        help=('Denoise strength. 0 for weak denoise (keep noise), 1 for strong denoise ability. '
-              'Only used for the realesr-general-x4v3 model'))
+        help='Denoise strength. 0 for weak denoise (keep noise), 1 for strong denoise ability. '
+              'Only used for the realesr-general-x4v3 model')
     parser.add_argument('-s', '--outscale', type=float, default=4, help='The final upsampling scale of the image')
-    parser.add_argument(
-        '--model_path', type=str, default=None, help='[Option] Model path. Usually, you do not need to specify it')
+    parser.add_argument('--model_path', type=str, default=None, help='[Option] Model path. Usually, you do not need to specify it')
     parser.add_argument('--suffix', type=str, default='out', help='Suffix of the restored image')
     parser.add_argument('-t', '--tile', type=int, default=0, help='Tile size, 0 for no tile during testing')
     parser.add_argument('--tile_pad', type=int, default=10, help='Tile padding')
     parser.add_argument('--pre_pad', type=int, default=0, help='Pre padding size at each border')
+    parser.add_argument('--pre_enhance', action='store_true', help='Enhance image before upscaling')
     parser.add_argument('--face_enhance', action='store_true', help='Use GFPGAN to enhance face')
+    parser.add_argument('--face_enhance_arch', type=str, default='CodeFormer', help='Face enhance arch: CodeFormer | GFPGAN | RestoreFormer | RestoreFormer++. Default: CodeFormer')
+    parser.add_argument('--face_enhance_model', type=str, default='1.4', help='Face enhance model for GFPGAN: 1.4 | 1.3 | 1.2. Default: 1.4')
+    parser.add_argument('--face_enhance_fidelity', type=float, default=1, help='Face enhance fidelity. Default: 1')
+    parser.add_argument('--face_upsample', action='store_true', help='Face upsample after enhancement. Default: False')
+    parser.add_argument('--face_upsample_model', type=str,
+        default='RealESRGAN_x4plus',
+        help='Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus | '
+              'realesr-animevideov3 | realesr-general-x4v3. Default: RealESRGAN_x4plus')
+    parser.add_argument('--face_offset', type=float, default=0, help='Face offset for more precise back alignment. Default: 0')
     parser.add_argument(
         '--fp32', action='store_true', help='Use fp32 precision during inference. Default: fp16 (half precision).')
     parser.add_argument(
@@ -55,55 +64,58 @@ def main():
     args = parser.parse_args()
 
     # determine models according to model names
-    args.model_name = args.model_name.split('.')[0]
-    if args.model_name == 'RealESRGAN_x4plus':  # x4 RRDBNet model
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-        netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
-    elif args.model_name == 'RealESRNet_x4plus':  # x4 RRDBNet model
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-        netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.1/RealESRNet_x4plus.pth']
-    elif args.model_name == 'RealESRGAN_x4plus_anime_6B':  # x4 RRDBNet model with 6 blocks
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
-        netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth']
-    elif args.model_name == 'RealESRGAN_x2plus':  # x2 RRDBNet model
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth']
-    elif args.model_name == 'realesr-animevideov3':  # x4 VGG-style model (XS size)
-        model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
-        netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-animevideov3.pth']
-    elif args.model_name == 'realesr-general-x4v3':  # x4 VGG-style model (S size)
-        model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
-        netscale = 4
-        file_url = [
-            'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-wdn-x4v3.pth',
-            'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth'
-        ]
+    def set_realesrgan(model_name):
+        if model_name == 'RealESRGAN_x4plus':  # x4 RRDBNet model
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+            netscale = 4
+            file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
+        elif model_name == 'RealESRNet_x4plus':  # x4 RRDBNet model
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+            netscale = 4
+            file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.1/RealESRNet_x4plus.pth']
+        elif model_name == 'RealESRGAN_x4plus_anime_6B':  # x4 RRDBNet model with 6 blocks
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
+            netscale = 4
+            file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth']
+        elif model_name == 'RealESRGAN_x2plus':  # x2 RRDBNet model
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
+            netscale = 2
+            file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth']
+        elif model_name == 'realesr-animevideov3':  # x4 VGG-style model (XS size)
+            model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
+            netscale = 4
+            file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-animevideov3.pth']
+        elif model_name == 'realesr-general-x4v3':  # x4 VGG-style model (S size)
+            model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
+            netscale = 4
+            file_url = [
+                'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-wdn-x4v3.pth',
+                'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth'
+            ]
 
-    # determine model paths
-    if args.model_path is not None:
-        model_path = args.model_path
-    else:
-        model_path = os.path.join('weights', args.model_name + '.pth')
-        if not os.path.isfile(model_path):
-            ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-            for url in file_url:
-                # model_path will be updated
-                model_path = load_file_from_url(
-                    url=url, model_dir=os.path.join(ROOT_DIR, 'weights'), progress=True, file_name=None)
+        # determine model paths
+        model_path = None
+        if args.model_path is not None:
+            model_path = args.model_path
+        else:
+            model_path = os.path.join('weights', model_name + '.pth')
+            if not os.path.isfile(model_path):
+                ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+                for url in file_url:
+                    # model_path will be updated
+                    model_path = load_file_from_url(url=url, model_dir=os.path.join(ROOT_DIR, 'weights'), progress=True, file_name=None)
 
-    # use dni to control the denoise strength
-    dni_weight = None
-    if args.model_name == 'realesr-general-x4v3' and args.denoise_strength != 1:
-        wdn_model_path = model_path.replace('realesr-general-x4v3', 'realesr-general-wdn-x4v3')
-        model_path = [model_path, wdn_model_path]
-        dni_weight = [args.denoise_strength, 1 - args.denoise_strength]
+        # use dni to control the denoise strength
+        dni_weight = None
+        if model_name == 'realesr-general-x4v3' and args.denoise_strength != 1:
+            wdn_model_path = model_path.replace('realesr-general-x4v3', 'realesr-general-wdn-x4v3')
+            model_path = [model_path, wdn_model_path]
+            dni_weight = [args.denoise_strength, 1 - args.denoise_strength]
 
-    # restorer
+        return (model_path, model, netscale, dni_weight)
+
+    # Image upsample
+    model_path, model, netscale, dni_weight = set_realesrgan(args.model_name.split('.')[0])
     upsampler = RealESRGANer(
         scale=netscale,
         model_path=model_path,
@@ -115,14 +127,50 @@ def main():
         half=not args.fp32,
         gpu_id=args.gpu_id)
 
+    # Face upsample
+    face_upsampler = None
+    if args.face_upsample:
+        model_path, model, netscale, dni_weight = set_realesrgan(args.face_upsample_model)
+        face_upsampler = RealESRGANer(
+            scale=netscale,
+            model_path=model_path,
+            dni_weight=dni_weight,
+            model=model,
+            tile=args.tile,
+            tile_pad=args.tile_pad,
+            pre_pad=args.pre_pad,
+            half=not args.fp32,
+            gpu_id=args.gpu_id)
+
+    # Face enhance
+    face_enhancer = None
     if args.face_enhance:  # Use GFPGAN for face enhancement
+        if args.face_enhance_arch == "GFPGAN":
+            args.face_enhance_arch = "clean"
+            if args.face_enhance_model == '1.4':
+                model_path = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth'
+            elif args.face_enhance_model == '1.3':
+                model_path = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
+            elif args.face_enhance_model == '1.2':
+                model_path = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.2.pth'
+        elif args.face_enhance_arch == "CodeFormer":
+            model_path = 'https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth'
+        elif args.face_enhance_arch == "RestoreFormer":
+            #model_path = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth'
+            model_path = 'https://github.com/wzhouxiff/RestoreFormerPlusPlus/releases/download/v1.0.0/RestoreFormer.ckpt'
+        elif args.face_enhance_arch == "RestoreFormer++":
+            model_path = 'https://github.com/wzhouxiff/RestoreFormerPlusPlus/releases/download/v1.0.0/RestoreFormer++.ckpt'
         from gfpgan import GFPGANer
         face_enhancer = GFPGANer(
-            model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth',
+            model_path=model_path,
+            pre_enhance=args.pre_enhance,
             upscale=args.outscale,
-            arch='clean',
+            arch=args.face_enhance_arch,
             channel_multiplier=2,
-            bg_upsampler=upsampler)
+            bg_upsampler=upsampler,
+            face_upsampler=face_upsampler,
+            face_offset=args.face_offset)
+
     os.makedirs(args.output, exist_ok=True)
 
     if os.path.isfile(args.input):
@@ -132,7 +180,7 @@ def main():
 
     for idx, path in enumerate(paths):
         imgname, extension = os.path.splitext(os.path.basename(path))
-        print('Testing', idx, imgname)
+        print('Processing', '[', str(idx).zfill(len(str(len(paths)))), '/', len(paths), ']', imgname)
 
         img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
         if len(img.shape) == 3 and img.shape[2] == 4:
@@ -142,8 +190,10 @@ def main():
 
         try:
             if args.face_enhance:
-                _, _, output = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True)
+                _, _, output = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True, weight=args.face_enhance_fidelity)
             else:
+                if args.pre_enhance:
+                    img = upsampler.enhance(img, outscale=1)[0]
                 output, _ = upsampler.enhance(img, outscale=args.outscale)
         except RuntimeError as error:
             print('Error', error)
--- inference_realesrgan_video.py
+++ inference_realesrgan_video.py
@@ -19,7 +19,7 @@ try:
     import ffmpeg
 except ImportError:
     import pip
-    pip.main(['install', '--user', 'ffmpeg-python'])
+    pip.main(['install', 'ffmpeg-python'])
     import ffmpeg
 
 
@@ -151,7 +151,7 @@ class Writer:
                                  pix_fmt='yuv420p',
                                  vcodec='libx264',
                                  loglevel='error',
-                                 acodec='copy').overwrite_output().run_async(
+                                 acodec='aac').overwrite_output().run_async(
                                      pipe_stdin=True, pipe_stdout=True, cmd=args.ffmpeg_bin))
         else:
             self.stream_writer = (
@@ -171,53 +171,58 @@ class Writer:
 
 
 def inference_video(args, video_save_path, device=None, total_workers=1, worker_idx=0):
-    # ---------------------- determine models according to model names ---------------------- #
-    args.model_name = args.model_name.split('.pth')[0]
-    if args.model_name == 'RealESRGAN_x4plus':  # x4 RRDBNet model
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-        netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
-    elif args.model_name == 'RealESRNet_x4plus':  # x4 RRDBNet model
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-        netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.1/RealESRNet_x4plus.pth']
-    elif args.model_name == 'RealESRGAN_x4plus_anime_6B':  # x4 RRDBNet model with 6 blocks
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
-        netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth']
-    elif args.model_name == 'RealESRGAN_x2plus':  # x2 RRDBNet model
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
-        netscale = 2
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth']
-    elif args.model_name == 'realesr-animevideov3':  # x4 VGG-style model (XS size)
-        model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
-        netscale = 4
-        file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-animevideov3.pth']
-    elif args.model_name == 'realesr-general-x4v3':  # x4 VGG-style model (S size)
-        model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
-        netscale = 4
-        file_url = [
-            'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-wdn-x4v3.pth',
-            'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth'
-        ]
+    def set_realesrgan(model_name):
+        if model_name == 'RealESRGAN_x4plus':  # x4 RRDBNet model
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+            netscale = 4
+            file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth']
+        elif model_name == 'RealESRNet_x4plus':  # x4 RRDBNet model
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+            netscale = 4
+            file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.1/RealESRNet_x4plus.pth']
+        elif model_name == 'RealESRGAN_x4plus_anime_6B':  # x4 RRDBNet model with 6 blocks
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=6, num_grow_ch=32, scale=4)
+            netscale = 4
+            file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth']
+        elif model_name == 'RealESRGAN_x2plus':  # x2 RRDBNet model
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
+            netscale = 2
+            file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth']
+        elif model_name == 'realesr-animevideov3':  # x4 VGG-style model (XS size)
+            model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=4, act_type='prelu')
+            netscale = 4
+            file_url = ['https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-animevideov3.pth']
+        elif model_name == 'realesr-general-x4v3':  # x4 VGG-style model (S size)
+            model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=32, upscale=4, act_type='prelu')
+            netscale = 4
+            file_url = [
+                'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-wdn-x4v3.pth',
+                'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth'
+            ]
 
-    # ---------------------- determine model paths ---------------------- #
-    model_path = os.path.join('weights', args.model_name + '.pth')
-    if not os.path.isfile(model_path):
-        ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-        for url in file_url:
-            # model_path will be updated
-            model_path = load_file_from_url(
-                url=url, model_dir=os.path.join(ROOT_DIR, 'weights'), progress=True, file_name=None)
+        # determine model paths
+        model_path = None
+        if args.model_path is not None:
+            model_path = args.model_path
+        else:
+            model_path = os.path.join('weights', model_name + '.pth')
+            if not os.path.isfile(model_path):
+                ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+                for url in file_url:
+                    # model_path will be updated
+                    model_path = load_file_from_url(url=url, model_dir=os.path.join(ROOT_DIR, 'weights'), progress=True, file_name=None)
 
-    # use dni to control the denoise strength
-    dni_weight = None
-    if args.model_name == 'realesr-general-x4v3' and args.denoise_strength != 1:
-        wdn_model_path = model_path.replace('realesr-general-x4v3', 'realesr-general-wdn-x4v3')
-        model_path = [model_path, wdn_model_path]
-        dni_weight = [args.denoise_strength, 1 - args.denoise_strength]
+        # use dni to control the denoise strength
+        dni_weight = None
+        if model_name == 'realesr-general-x4v3' and args.denoise_strength != 1:
+            wdn_model_path = model_path.replace('realesr-general-x4v3', 'realesr-general-wdn-x4v3')
+            model_path = [model_path, wdn_model_path]
+            dni_weight = [args.denoise_strength, 1 - args.denoise_strength]
 
-    # restorer
+        return (model_path, model, netscale, dni_weight)
+
+    # Image upsample
+    model_path, model, netscale, dni_weight = set_realesrgan(args.model_name.split('.')[0])
     upsampler = RealESRGANer(
         scale=netscale,
         model_path=model_path,
@@ -230,21 +235,51 @@ def inference_video(args, video_save_path, device=None, total_workers=1, worker_
         device=device,
     )
 
+    # Face upsample
+    face_upsampler = None
+    if args.face_upsample:
+        model_path, model, netscale, dni_weight = set_realesrgan(args.face_upsample_model)
+        face_upsampler = RealESRGANer(
+            scale=netscale,
+            model_path=model_path,
+            dni_weight=dni_weight,
+            model=model,
+            tile=args.tile,
+            tile_pad=args.tile_pad,
+            pre_pad=args.pre_pad,
+            half=not args.fp32,
+            gpu_id=args.gpu_id)
+
+    # Face enhance
+    face_enhancer = None
     if 'anime' in args.model_name and args.face_enhance:
         print('face_enhance is not supported in anime models, we turned this option off for you. '
               'if you insist on turning it on, please manually comment the relevant lines of code.')
         args.face_enhance = False
-
     if args.face_enhance:  # Use GFPGAN for face enhancement
+        if args.face_enhance_arch == "GFPGAN":
+            args.face_enhance_arch = "clean"
+            if args.face_enhance_model == '1.4':
+                model_path = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth'
+            elif args.face_enhance_model == '1.3':
+                model_path = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
+            elif args.face_enhance_model == '1.2':
+                model_path = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.2.pth'
+        elif args.face_enhance_arch == "CodeFormer":
+            model_path = 'https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth'
+        elif args.face_enhance_arch == "RestoreFormer":
+            #model_path = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth'
+            model_path = 'https://github.com/wzhouxiff/RestoreFormerPlusPlus/releases/download/v1.0.0/RestoreFormer.ckpt'
+        elif args.face_enhance_arch == "RestoreFormer++":
+            model_path = 'https://github.com/wzhouxiff/RestoreFormerPlusPlus/releases/download/v1.0.0/RestoreFormer++.ckpt'
         from gfpgan import GFPGANer
         face_enhancer = GFPGANer(
-            model_path='https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth',
+            model_path=model_path,
             upscale=args.outscale,
-            arch='clean',
+            arch=args.face_enhance_arch,
             channel_multiplier=2,
-            bg_upsampler=upsampler)  # TODO support custom device
-    else:
-        face_enhancer = None
+            bg_upsampler=upsampler,
+            face_upsampler=face_upsampler)
 
     reader = Reader(args, total_workers, worker_idx)
     audio = reader.get_audio()
@@ -260,7 +295,7 @@ def inference_video(args, video_save_path, device=None, total_workers=1, worker_
 
         try:
             if args.face_enhance:
-                _, _, output = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True)
+                _, _, output = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True, weight=args.face_enhance_fidelity)
             else:
                 output, _ = upsampler.enhance(img, outscale=args.outscale)
         except RuntimeError as error:
@@ -330,15 +365,14 @@ def main():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', type=str, default='inputs', help='Input video, image or folder')
+    parser.add_argument('-o', '--output', type=str, default='outputs', help='Output folder')
     parser.add_argument(
         '-n',
         '--model_name',
         type=str,
-        default='realesr-animevideov3',
-        help=('Model names: realesr-animevideov3 | RealESRGAN_x4plus_anime_6B | RealESRGAN_x4plus | RealESRNet_x4plus |'
-              ' RealESRGAN_x2plus | realesr-general-x4v3'
-              'Default:realesr-animevideov3'))
-    parser.add_argument('-o', '--output', type=str, default='results', help='Output folder')
+        default='RealESRGAN_x4plus',
+        help='Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus | '
+              'realesr-animevideov3 | realesr-general-x4v3. Default: RealESRGAN_x4plus')
     parser.add_argument(
         '-dn',
         '--denoise_strength',
@@ -347,11 +381,21 @@ def main():
         help=('Denoise strength. 0 for weak denoise (keep noise), 1 for strong denoise ability. '
               'Only used for the realesr-general-x4v3 model'))
     parser.add_argument('-s', '--outscale', type=float, default=4, help='The final upsampling scale of the image')
+    parser.add_argument('--model_path', type=str, default=None, help='[Option] Model path. Usually, you do not need to specify it')
     parser.add_argument('--suffix', type=str, default='out', help='Suffix of the restored video')
     parser.add_argument('-t', '--tile', type=int, default=0, help='Tile size, 0 for no tile during testing')
     parser.add_argument('--tile_pad', type=int, default=10, help='Tile padding')
     parser.add_argument('--pre_pad', type=int, default=0, help='Pre padding size at each border')
     parser.add_argument('--face_enhance', action='store_true', help='Use GFPGAN to enhance face')
+    parser.add_argument('--face_enhance_arch', type=str, default='CodeFormer', help='Face enhance arch:  CodeFormer | GFPGAN | RestoreFormer | RestoreFormer++. Default: CodeFormer')
+    parser.add_argument('--face_enhance_model', type=str, default='1.4', help='Face enhance model for GFPGAN: 1.4 | 1.3 | 1.2. Default: 1.4')
+    parser.add_argument('--face_enhance_fidelity', type=float, default=1, help='Face enhance fidelity. Default: 1')
+    parser.add_argument('--face_upsample', action='store_true', help='Face upsample after enhancement. Default: False')
+    parser.add_argument('--face_upsample_model', type=str,
+        default='RealESRGAN_x4plus',
+        help='Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus | '
+              'realesr-animevideov3 | realesr-general-x4v3. Default: RealESRGAN_x4plus')
+
     parser.add_argument(
         '--fp32', action='store_true', help='Use fp32 precision during inference. Default: fp16 (half precision).')
     parser.add_argument('--fps', type=float, default=None, help='FPS of the output video')
@@ -369,6 +413,9 @@ def main():
         type=str,
         default='auto',
         help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs')
+    parser.add_argument(
+        '-g', '--gpu-id', type=int, default=None, help='gpu device to use (default=None) can be 0,1,2 for multi-gpu')
+
     args = parser.parse_args()
 
     args.input = args.input.rstrip('/').rstrip('\\')
